a250571b83f730debfcf57154398571525b73b64
Marckitos610e <ma.paz24@info.uas.edu.mx>
2026-05-30 00:43:56 -0700

    Fix SSL para Neon

--- database.js
+++ database.js
@@ -1,25 +1,30 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-require('dotenv').config(); 
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
+
 async function conectar(){
   try{
     await sequelize.authenticate();
-    console.log('Conexión éxitosa a la base de datos.');
+    console.log('Conexion exitosa a la base de datos.');
   }catch(error){
-    console.error('Error de conexión',error);
+    console.error('Error de conexion', error);
   }
 }
 
 conectar();
-
-module.exports =  sequelize ;
-
+module.exports = sequelize;
